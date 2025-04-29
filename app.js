8f5fc592bb9cfd5a774aa0da89e224f22be58370
Giulia Belatini <143298286+GiuliaaaB@users.noreply.github.com>
2025-04-29 15:34:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
-
 function pesquisar() {
-    // Obtém a seção HTML onde os resultados serão exibidos
-    let section = document.getElementById("resultados-pesquisa");
+    let section = document.getElementById("item-resultado");
+    let campoPesquisa = document.getElementById("campo-pesquisa").value.trim();
 
-    let campoPesquisa = document.getElementById("campo-pesquisa").value
-
-    // se campoPesquisa for uma string sem nada
     if (!campoPesquisa) {
-        section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar o nome de uma série.</p>"
+        section.innerHTML = "<p> Nada foi encontrado. Digite algo como 'pequeno', 'fofo' ou 'brincalhão' para começar sua busca!</p>"
         return 
     }
 
     campoPesquisa = campoPesquisa.toLowerCase()
+    document.getElementById("campo-pesquisa").style.backgroundColor = "#f8b5ff"; 
 
-    // Inicializa uma string vazia para armazenar os resultados
     let resultados = "";
     let titulo = ""; 
     let descricao = "";
@@ -25,42 +21,44 @@ function pesquisar() {
     let origem = "";
     let image = "";
 
-    // Itera sobre cada dado da lista de dados
     for (let dado of dados) {
-        titulo = dado.titulo.toLowerCase()
-        descricao = dado.descricao.toLowerCase()
-        tags = dado.tags.toLowerCase()
-        personalidade = dado.personalidade.toLowerCase()
-        porte = dado.porte.toLowerCase()
-        peso = dado.peso.toLowerCase()
-        vida = dado.vida.toLowerCase()
-        origem = dado.origem.toLowerCase()
-        
-        // se titulo includes campoPesquisa
+        titulo = (dado.titulo || "").toLowerCase();
+        descricao = (dado.descricao || "").toLowerCase();
+        tags = (dado.tags || "").toLowerCase();
+        personalidade = (dado.personalidade || "").toLowerCase();
+        porte = (dado.porte || "").toLowerCase();
+        peso = (dado.peso || "").toLowerCase();
+        vida = (dado.vida || "").toLowerCase();
+        origem = (dado.origem || "").toLowerCase();
+
         if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa) || personalidade.includes(campoPesquisa) || porte.includes(campoPesquisa) ||  peso.includes(campoPesquisa) || vida.includes(campoPesquisa) || origem.includes(campoPesquisa)) {
-            // cria um novo elemento
+            
             resultados += `
-            <div class="item-resultado">
-                <h2>
-                    <a href="" target="_blank">${dado.titulo}</a>
-                </h2>
-                <p class="descricao-meta">${dado.descricao}</p>
-                <p class="personalidade">${dado.personalidade}</p>
-                <p class="porte">${dado.porte}</p>
-                <p class="peso">${dado.peso}</p>
-                <p class="vida"> ${dado.vida}</p>
-                <p class="origem"> ${dado.origem}</p>
-                <img src="${dado.image}" alt="${dado.titulo}" style="width: 50%; height: auto;">
+            <div class="card-raca">
+                <div class="conteudo-texto">
+                <h2>${dado.titulo}</h2>
+                <p>${dado.descricao}</p>
+                <div class="linha-infos">
+                <p><strong>Porte:</strong> ${dado.porte}</p>
+                <p><strong>Peso:</strong> ${dado.peso}</p>
             </div>
-        `;
+            <div class="linha-infos">
+                <p><strong>Expectativa de vida:</strong> ${dado.vida}</p>
+                <p><strong>Origem:</strong> ${dado.origem}</p>
+            </div>
+            </div>
+                <div class="imagem-cachorro">
+                <img src="${dado.image}" alt="${dado.titulo}">
+            </div>
+            </div>
+            `;
+            
         }
     }
 
     if (!resultados) {
-        resultados = "<p>Nada foi encontrado</p>"
+        resultados = "<p>Nada foi encontrado.</p>"
     }
 
-    // Atribui os resultados gerados à seção HTML
     section.innerHTML = resultados;
-    const img = document.createElement('img');
 }
